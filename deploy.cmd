8429e528e8e459b2e4a790484df532943e5e2300
Bharat Tiwari <kkaphm@live.com>
2017-06-25 00:43:55 -0500

    updated start script to just run node build\app.js, tsc is moved in deploy.cmd as part of building process command

--- deploy.cmd
+++ deploy.cmd
@@ -108,6 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   ::call :ExecuteCmd !NPM_CMD! install --production
   call :ExecuteCmd !NPM_CMD! install
+  call :ExecuteCmd !NPM_CMD! tsc
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
