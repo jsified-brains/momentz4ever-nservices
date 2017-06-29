6ac53bb3168c5ba1bef8bc42ef9581729eb58e63
Graphicaction <rksharma2679@gmail.com>
2017-06-28 20:03:38 -0400

    Updated Users and Albums files with all CRUD operations developed

--- src/users.ts
+++ src/users.ts
@@ -21,11 +21,53 @@ router.get('/users',function(req,res){
     res.send(myUsers);
 });
 
+router.get('/users/:id',function(req,res){
+    console.log(req.params.id);
+    let userWithId=myUsers.filter(function(user){
+        return user.id===req.params.id;
+    });
+    if(userWithId.length===0){
+        res.send("User Not Found");
+    }
+    res.send(userWithId[0]);
+});
+
+router.delete('/users',function(req,res){
+    myUsers=[];
+    res.send({"result":"All Users deleted Successfully",
+    "updatedUsers":myUsers});
+});
+
+router.delete('/users/:id',function(req,res){
+    console.log(req.params.id);
+    let userWithId=myUsers.filter(function(user){
+        return user.id!==req.params.id;
+    });
+       myUsers = userWithId;
+    res.send({"result":"User deleted Successfully",
+    "updatedUsers":myUsers});
+});
+
 router.post('/users',function(req,res){
      let newUser=req.body;
      myUsers.push(newUser);
-     res.send({"result":"User Added Sussesfully",
+     res.send({"result":"User Added Successfully",
     "updatedUsers":myUsers});
+});
+
+router.put('/users/:id', function (req, res) {
+    for (var i = 0; i < myUsers.length; i++) {
+        if (myUsers[i].id === req.params.id) {
+            myUsers[i].id = req.params.id;
+            myUsers[i].firstName = req.body.firstName;
+            myUsers[i].lastName = req.body.lastName;
+            myUsers[i].email = req.body.email;
+            res.send(myUsers[i]);
+        }
+        else {
+            res.send("User Not Found");
+        }
+    }
 });
 
 module.exports = router;
